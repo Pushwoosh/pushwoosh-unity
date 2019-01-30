3b4b28fc3b18717bbc2a01d3917fa14baeb4c13f
Alexander Evstefeev <aevstefeev@pushwoosh.com>
2019-01-30 18:45:16 +0700

    [feature][PUSH-17482] add script for copy google-play.json to Plugins/assets

---
+++ PushwooshUnitySample/Assets/Editor/CustomBuildProcessor.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+class MyCustomBuildProcessor : IPreprocessBuild
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildTarget target, string path)
+    {
+        string assetsUrl = System.IO.Directory.GetCurrentDirectory()+"/Assets/";
+        string sourcePath = assetsUrl + "google-services.json";
+        if (File.Exists(sourcePath))
+        {
+            FileUtil.CopyFileOrDirectory(sourcePath, assetsUrl + "/Plugins/Android/assets/google-services.json");
+        }
+    }
+}
+#endif
