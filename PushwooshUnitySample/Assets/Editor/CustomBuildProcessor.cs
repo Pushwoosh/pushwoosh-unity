5fbed6813b3c5aa9a245d08fe06dacec0149fd59
Александр <aleksandr@alex.ev.must_run>
2019-02-09 00:12:35 +0700

    [bugfix][PUSH-17873] convert json to xml

--- PushwooshUnitySample/Assets/Editor/CustomBuildProcessor.cs
+++ PushwooshUnitySample/Assets/Editor/CustomBuildProcessor.cs
@@ -1,5 +1,8 @@
 ﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using System.IO;
+using System.Xml;
+using MiniJSON;
 using UnityEditor;
 using UnityEditor.Build;
 class MyCustomBuildProcessor : IPreprocessBuild
@@ -9,10 +12,50 @@ class MyCustomBuildProcessor : IPreprocessBuild
     {
         string assetsUrl = System.IO.Directory.GetCurrentDirectory() + "/Assets/";
         string sourcePath = assetsUrl + "google-services.json";
+        string destenishinPath = assetsUrl + "/Plugins/Android/res/values/googleservices.xml";
         if (File.Exists(sourcePath))
         {
-            FileUtil.CopyFileOrDirectory(sourcePath, assetsUrl + "/Plugins/Android/assets/google-services.json");
+    
+           //Read the text from directly from the test.txt file
+           string json = readJson(sourcePath);
+            var dict = Json.Deserialize(json) as Dictionary<string, object>;
+            var project_info = dict["project_info"] as Dictionary<string, object>;
+            var project_number = project_info["project_number"] as string;
+
+
+            var client = dict["client"] as List<object>;
+            var element = client[0] as Dictionary<string, object>;
+            var oauthClient = element["oauth_client"] as List<object>;
+            var elementOathClient = oauthClient[0] as Dictionary<string, object>;
+            var clientId = elementOathClient["client_id"] as string;
+
+            var clientInfo = element["client_info"] as Dictionary<string, object>;
+            var appId = clientInfo["mobilesdk_app_id"];
+             
+
+            var xml =
+            "<?xml version='1.0' encoding='utf-8'?>\n<resources tools:keep=\"@string/gcm_defaultSenderId,@string/project_id,@string/default_web_client_id,@string/google_app_id,\" xmlns:tools=\"http://schemas.android.com/tools\">\n "
+            + "<string name=\"gcm_defaultSenderId\" translatable=\"false\">" + project_number + "</string>\n"
+            + "<string name=\"default_web_client_id\" translatable=\"false\">" + clientId + "</string>\n"
+            + "<string name=\"google_app_id\" translatable=\"false\">" + appId + "</string>\n"
+            + "</resources>";
+
+            writeFile(xml, destenishinPath);
         }
+    }
+
+    string readJson(string path) {
+        StreamReader reader = new StreamReader(path);
+        string json = reader.ReadToEnd();
+        reader.Close();
+        return json;
+    }
+
+    void writeFile(string content, string path)
+    {
+        StreamWriter writer = new StreamWriter(path, false);
+        writer.WriteLine(content);
+        writer.Close();
     }
 }
 #endif
