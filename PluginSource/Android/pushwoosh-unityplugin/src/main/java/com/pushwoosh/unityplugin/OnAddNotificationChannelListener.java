4c24831dfe358938e80eee84c2bf484d174f8571
Nikolay Galizin <ngalizin@pushwoosh.com>
2019-04-12 19:12:31 +0700

    fix javadoc in OnAddNotificationChannelListener class

--- PluginSource/Android/pushwoosh-unityplugin/src/main/java/com/pushwoosh/unityplugin/OnAddNotificationChannelListener.java
+++ PluginSource/Android/pushwoosh-unityplugin/src/main/java/com/pushwoosh/unityplugin/OnAddNotificationChannelListener.java
@@ -3,14 +3,14 @@ package com.pushwoosh.unityplugin;
 public interface OnAddNotificationChannelListener {
     /**
      * @param channelName name of the channel specified in Android payload as "pw_channel" attribute.
-     * If no attribute was specified, returns default channel name
+     * If no attribute was specified, parameter gives default channel name
      * @return name that you want to assign to the channel on its creation. Note that empty name
      * will be ignored and default channel name will be assigned to the channel instead
      */
     String channelName(String channelName);
     /**
      * @param channelName name of the channel specified in Android payload as "pw_channel" attribute.
-     * If no attribute was specified, returns default channel name
+     * If no attribute was specified, parameter gives default channel name
      * @return description that you want to assign to the channel on its creation
      */
     String channelDescription(String channelName);
